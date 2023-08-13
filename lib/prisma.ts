8c2ac2d72d737d5ca40d96a6f18c15b1d568d021
Tea <100817706+TeaByte@users.noreply.github.com>
2023-08-13 18:24:11 +0400

    prisma.ts: added createUserOrUpdate

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -3,11 +3,17 @@ import { PrismaClient } from "@prisma/client";
 
 export const prisma = new PrismaClient();
 
-export async function createUser(user: TelegramUserData) {
-	console.log(user);
-	return prisma.user.create({
-		data: {
+export async function createUserOrUpdate(user: TelegramUserData) {
+	return prisma.user.upsert({
+		where: {
 			id: user.id.toString(),
+		},
+		create: {
+			id: user.id.toString(),
+			name: user.first_name,
+			image: user.photo_url,
+		},
+		update: {
 			name: user.first_name,
 			image: user.photo_url,
 		},
